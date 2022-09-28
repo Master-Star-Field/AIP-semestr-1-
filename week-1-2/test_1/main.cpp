6cb9cbf3e09ac993e93c7d309a1489b02a5af3fb
Master-Star-Field <113855899+Master-Star-Field@users.noreply.github.com>
2022-09-29 00:19:19 +0300

    hello

---
+++ week-1-2/test_1/main.cpp
@@ -0,0 +1,21 @@
+//* Вывести на экран Первую букву имени из символов "*" (звездочка)
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "****   " << std::endl;
+    std::cout << "*    *" << std::endl;
+    std::cout << "*    *" << std::endl;
+    std::cout << "*****  " << std::endl;
+    std::cout << "*    * " << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "****** " << std::endl;
+
+    getchar();
+    getchar();
+
+    return 0;
+}
+
